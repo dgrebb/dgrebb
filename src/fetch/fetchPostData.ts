3fcc2b79cc8a2470893fb728bd1bd0167024d44c
Dan Grebb <dan@dgrebb.com>
2023-12-25 01:45:50 -0500

    feat(github): adds README generator and workflow

---
+++ src/fetch/fetchPostData.ts
@@ -0,0 +1,22 @@
+import Parser from "rss-parser";
+const parser = new Parser();
+
+export async function fetchPostData(url: string): Promise<string> {
+  const feed = await parser.parseURL(url);
+
+  const list = feed.items.slice(0, 5).map((item) => {
+    const date = new Date(item.pubDate as string);
+    const publishedDate = `${date.toLocaleString("en-us", {
+      month: "long",
+      year: "numeric",
+    })}`;
+
+    return `<li><a href=${item.link} target="_blank" rel="noopener noreferrer" style="color: #A52A2A;">${item.title}</a> (${publishedDate})</li>`;
+  });
+
+  return `
+  <ul>
+    ${list.join("")}
+  </ul>\n
+  <!-- <p>» <a href="https://www.dgrebb.com/posts" style="color: #A52A2A;">Read more posts</a></p> -->`
+}
